2d8d7dcbc2a1c892de93dc2cdeea8ce60d489505
Rob Taylor <rob@uxblondon.com>
2014-11-14 16:32:57 +0000

    Initial commit

---
+++ src/Twitter/LatestTweets.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Twitter;
+
+use TwitterOAuth\Api as TwitterApi;
+
+class LatestTweets
+{
+    /**
+     *
+     * @var TwitterApi
+     */
+    private $api;
+
+
+    /**
+     *
+     * @param string $apiKey
+     * @param string $apiSecret
+     */
+    public function __construct(TwitterApi $api)
+    {
+        $this->api = $api;
+    }
+
+    public function getLatestTweet($username)
+    {
+        $tweets = $this->getTweets($userame, 1);
+
+        return isset($tweets[0]) ? $tweets[0] : null;
+    }
+
+    public function getTweets($username, $howMany)
+    {
+        $tweets = $this->api->get('statuses/user_timeline', array(
+            'screen_name' => $username,
+            'exclude_replies' => 'true',
+            'include_rts' => 'false',
+            'count' => $howMany
+        ));
+
+        array_walk($tweets, function(&$tweet) {
+            $tweet = new Tweet($tweet);
+        });
+
+        return $tweets;
+    }
+}
